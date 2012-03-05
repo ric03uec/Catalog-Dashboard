14b58680de985c8f82975e7760ac4a2bdb76f042
devashish <devashish.86@gmail.com>
2012-03-05 22:58:22 +0530

    completed git add views/linkCol.jade

--- models/catalog.js
+++ models/catalog.js
@@ -2,6 +2,7 @@
 
 var util = require('util');
 var db = require('./db');
+var Moment = require('moment');
 
 var catalog = {};
 
@@ -9,6 +10,12 @@ catalog.getAll = function(callback){
   var queryStr = 'select * from entries ';
   db.query(queryStr, function(err, results){
     if(!err && results){
+      for(var i = 0 ; i < results.length ; i++){
+        var date = results[i]['stock_arrival_date'];
+        date = Moment(date+ '', "ddd, DD MMM YYYY hh:mm:ss zz")
+        date = Moment(date).format('DD-MM-YYYY');
+        results[i]['stock_arrival_date'] = date;
+      }
       callback(null, results);
     }else{
       util.error('Error fetching all the entries from DB : ' + err);
