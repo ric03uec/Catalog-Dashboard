152effc504543883f767e300e7acd90565196a19
devashish <devashish.86@gmail.com>
2012-03-06 01:30:36 +0530

    few more date fixes

--- routes/main.js
+++ routes/main.js
@@ -9,17 +9,33 @@ var catalog = require('../models/catalog');
 
 var main = {};
 
+var convertDate = function(dateStr){
+  if(null !== dateStr){
+    var date = Moment(dateStr + '', "ddd, DD MMM YYYY hh:mm:ss zz");
+    date = Moment(date).format('DD-MM-YYYY');
+    return date;
+  }else{
+    return 'Incomplete';
+  }
+};
+
 main.show = function(req, res){
   var queryStr = 'select * from entries where id =' + req.params.id + ';';
   db.query(queryStr, function(err, results){
     var entry = results[0]; 
-    var date = entry['stock_arrival_date'];
-    date = Moment(date + '', "ddd, DD MMM YYYY hh:mm:ss zz");
-    date = Moment(date).format('DD-MM-YYYY');
-    entry['stock_arrival_date'] = date;
+    entry['stock_arrival_date'] = convertDate(entry['stock_arrival_date']);
+    entry['content_assign_date'] = convertDate(entry['content_assign_date']);
+    entry['catalog_submission_date'] = convertDate(entry['catalog_submission_date']);
+    entry['photo_complete_date'] = convertDate(entry['photo_complete_date']);
+    entry['image_assignment_date']=convertDate(entry['image_assignment_date']);
+    entry['editing_completion_date']=convertDate(entry['editing_completion_date']);
+    entry['approved_date'] = convertDate(entry['approved_date']);
+    entry['launch_date'] = convertDate(entry['launch_date']);
+
     res.render('showEntry' , {title : 'catalog Dashboard','entry': entry}); 
   });
 };
+
 
 module.exports = main;
 //-------------------------------------//
